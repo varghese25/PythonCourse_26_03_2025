3a394e667d7df62aa60de8e70571e9a12c40f9aa
varghese25 <vbpoulose@gmail.com>
2025-03-26 14:37:13 -0400

    String Handling

---
+++ String.py
@@ -0,0 +1,28 @@
+
+#method, Function - performs a specified task
+
+message = 'happ birthday'
+name = " Varghese"
+quote = '"peace"'
+print(name)
+print(quote)
+print(name.upper()) # print is Function & upper() is a method
+print(name.lower()) # dot (.upper()) invoking a method
+
+
+print(message.title())
+print(message +  name)
+
+
+msg = message + name
+
+
+#escape sequence
+print("hello \n world")
+print("hello \t varghese")
+
+print(len(message)) # find length
+print(message.find("h")) # index of h is Zero
+
+'''Watch from 36.58'''
+
