9b3a240958e405e4e3076bbbb2af83ac76b0a6a6
varghese25 <vbpoulose@gmail.com>
2025-03-31 17:36:48 -0400

    Day 2

--- Code_Assignment.py
+++ Code_Assignment.py
@@ -1,6 +1,36 @@
-
-
 #Day 1 Assignment
-name, days, year = "Anand", 15, 2025
+# name, days, year = "Anand", 15, 2025
+#
+# print("Dear, " + str(name) + " you have " + str(days) + " of leave balance for this " + str(year) + ".")
 
-print("Dear, " + str(name) + " you have " + str(days) + " of leave balance for this " + str(year) + ".")
+
+#Day 2  Assignment (UserInput)
+
+# Get User ID
+# Name, EmailID, and PhoneNumber,
+# print the infromation:
+# UserName:
+# Email:
+# PhoneNumber:
+
+# name = input("Enter your name: ")
+# email = input("enter your email:")
+# phoneNumber = input("enter number:")
+# print("******************************")
+# print("userName :" + name)
+# print("Email:" + email)
+# print("ph :" + phoneNumber)
+# print("******************************")
+
+
+# Get User
+# Get a number n from user. compute and print the following values #
+#1. log2(n)
+#2. cos(n)
+#3. en#
+
+import math
+n = 5
+print(math.log2(n)) #
+print(math.cos(n))
+print(math.exp(n))
