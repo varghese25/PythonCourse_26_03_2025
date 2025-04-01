9a622eb11bb64467bb6c2c97ff111540f862bb66
varghese25 <vbpoulose@gmail.com>
2025-04-01 12:29:53 -0400

    Day 2

---
+++ ifElse.py
@@ -0,0 +1,18 @@
+pwd_correct = False
+
+if pwd_correct: #conditional stmt
+    print("Logged In")
+    print("Have a Nice Day")
+else:
+    print("Incorrect Pwd")
+    print("Try Again")
+
+print("Bye")
+
+
+# num = 20
+num = int(input("enter the number: "))
+if num % 10 == 0:
+    print(str(num) + " is a multiple of 10")
+else:
+    print(str(num) + " is not a multiple of 10")
