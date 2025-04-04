33b01ecb06dcf4e5d8c74f3814099a17789b7978
varghese25 <vbpoulose@gmail.com>
2025-04-03 20:35:42 -0400

    Day 4 bitWise Operators

---
+++ bitwseOperator.py
@@ -0,0 +1,17 @@
+# Bitwise
+'''and
+| or
+~ not
+^ exor
+>> left shift
+<< right shift
+'''
+
+#7 is byte similary every number is byte
+#0001 is bit , Ex 01 & 01 each bit will operation is bitwise operator
+
+#a b    a & b
+#0 0
+#
+
+#Continue from 1:36.27
