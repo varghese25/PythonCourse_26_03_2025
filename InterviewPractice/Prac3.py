bc6ad1bac9b1e0d0fbb0f186db8e536a10c5e574
Varghese Baby <Vbpoulose@gmail.com>
2025-08-19 12:46:35 -0400

    Py:Prac3_if_else

---
+++ InterviewPractice/Prac3.py
@@ -0,0 +1,15 @@
+x = 7
+if x > 5:
+    if x < 10:
+        print("A")
+    else: 
+        print("B")
+else:
+        print("C")
+
+"""
+First the program checks the if x is greater than 5.
+Since x is 7, this is true. Then ,inside that block,it checks
+if x is less than 10. Again, 7 is less tha 10, so this is also true.
+Therefore, the program prints A. 
+"""
