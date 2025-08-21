dd6e5927179913e76097692e58ac13d2256cf46f
Varghese Baby <Vbpoulose@gmail.com>
2025-08-21 19:54:30 -0400

    Py:Prac8:Tree-Patten.

--- InterviewPractice/Prac8.py
+++ InterviewPractice/Prac8.py
@@ -9,11 +9,13 @@
 # height is 6
 # 6 *2 = 12 - 1 = 11 width of the tree
 def tree(height):
-    length = height * 2 - 1
-    stars = 1
-    for i in range(1, height + 1):
-        print(("*" * stars).center(length))
-        stars += 2
+    length = height * 2 - 1 # total width of the tree (max stars in the bottom row)
+    stars = 1 # start with 1 star at the top
+    for i in range(1, height + 1):  # loop through tree height
+        print(("*" * stars).center(length)) # print stars, centered
+        stars += 2  # increase stars by 2 each row (1, 3, 5, …)
+
+         # Print the trunk (just 1 star, centered)
     print("*".center(length))
 
 tree(9)  # Add this line to call the function
