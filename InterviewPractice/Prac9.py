df8d5f7d853adbe641952fd091ab7a64f91cddf6
Varghese Baby <Vbpoulose@gmail.com>
2025-08-23 17:53:39 -0400

    Py: Repeats-List

---
+++ InterviewPractice/Prac9.py
@@ -0,0 +1,4 @@
+x = [1, 2, 3] 
+print(x * 2)
+
+# it repeats the whole list
--- InterviewPractice/tempCodeRunnerFile.py
+++ InterviewPractice/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-def tree(height):
-    length = height * 2 - 1
-    stars = 1
-    for i in range(1, height + 1):
-        print(("*" * stars).center(length))
-        stars += 2
-    # Print the trunk
-    print("*".center(length))
+console.log([].toString())
