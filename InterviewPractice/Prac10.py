0652482d8d9034d86dcca161b7a90f8ff671e11e
Varghese Baby <Vbpoulose@gmail.com>
2025-08-24 17:32:02 -0400

    Py: Prac10-Triangle-KeyWord-def-funcName-param-colon-Indented

---
+++ InterviewPractice/Prac10.py
@@ -0,0 +1,40 @@
+# def print_triangle(n): #🔑 Rule: After a colon :, Python expects an indented block of code.
+#     for i in range(n):
+#      print("*" * i)
+
+
+# print_triangle(14)
+
+
+#⚡ Rule of thumb:Every time you see a colon (:), the very next line must be indented.
+
+"""
+Where you’ll see colons (:) in Python
+
+if, elif, else
+
+for, while
+
+def (functions)
+
+class
+
+try, except, finally
+
+with
+"""
+
+
+# def greet(name):
+#     print('Hello ' + name + ', how are your? ')
+
+# greet('Varghese')
+
+
+# def → tells Python you are defining a function
+
+# greet → function name
+
+# (name) → parameter (input to the function)
+
+# : → starts the function block, so the next line must be indented
