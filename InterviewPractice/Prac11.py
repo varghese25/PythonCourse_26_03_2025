ad829898634891b2e8419ee6f98880f102688189
Varghese Baby <Vbpoulose@gmail.com>
2025-08-25 18:54:12 -0400

    Py: Prac11-zipFunction

---
+++ InterviewPractice/Prac11.py
@@ -0,0 +1,6 @@
+list1 =[10,20,30]
+list2 =['x','y','z']
+result = list(zip(list1,list2))
+print(result)
+
+# The zip() function pair elements from both list into tuples, resulting in a list of paried tuples.
