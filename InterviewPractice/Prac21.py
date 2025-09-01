98ab7683d1a17d4497b3c6ef85f0356912b44198
Varghese Baby <Vbpoulose@gmail.com>
2025-09-01 19:29:23 -0400

    Py: None-Returns

---
+++ InterviewPractice/Prac21.py
@@ -0,0 +1,18 @@
+d = {"a": 1} # Dictionary with one key-value pair
+print(d.update({"a":2}))
+
+
+"""
+The Update() method modifies the dictionary d 
+in place, changing the value  "a" to 2, but it does returns None.
+The print() function outputs this return value.
+
+"""
+
+
+# # Function with no return
+# def say_hello():
+#     print("Hello")
+
+# result = say_hello()
+# print("Result:", result)
