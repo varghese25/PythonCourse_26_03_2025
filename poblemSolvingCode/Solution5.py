f4dbd8651ef799768b172b5223da36c108417f89
Varghese Baby <Vbpoulose@gmail.com>
2025-09-15 19:03:36 -0400

    Py: interSection(&), Union(|)

---
+++ poblemSolvingCode/Solution5.py
@@ -0,0 +1,15 @@
+s1 ={1,2,3}
+s2={3,4,5}
+print(s1 & s2, s1|s2) #intersection and union
+
+"""
+
+
+
+"&" means common values (intersection) (3)
+"|" means all unique values (union) (1,2,3,4,5)
+
+
+Output: {3} {1, 2, 3, 4, 5}
+
+"""
