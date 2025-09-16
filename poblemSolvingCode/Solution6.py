aaa4f8c191345df0841682796ed181e1a2ab99ab
Varghese Baby <Vbpoulose@gmail.com>
2025-09-16 19:24:43 -0400

    Py: The *operaters Usage

---
+++ poblemSolvingCode/Solution6.py
@@ -0,0 +1,7 @@
+x,y,*z=[10,20,30,40]
+print(x,y,z) 
+
+# The  * operaters collects the remaining values into a list.
+# x =10
+# y = 20
+# z = [30,40] * operaters collects the remaining values into a list.
