8db30074ff17d31a39c4e117d63223e27f2ee942
Varghese Baby <Vbpoulose@gmail.com>
2025-09-19 17:55:30 -0400

    Py: LowerCase-UpperCase

---
+++ poblemSolvingCode/Solution8.py
@@ -0,0 +1,18 @@
+x = 'VARGHESE BABY'
+name = (x.lower())
+print(name + ' lower case')
+
+print(name.title() + ' title case')
+print(name.upper() + ' upper case')
+print(name.capitalize() + ' capitalize case')
+print(name.swapcase() + ' swap case')
+print(name.center(30, '*') + ' center case')
+print(name.ljust(30, '*') + ' left justify case')
+print(name.rjust(30, '*') + ' right justify case')
+print(name.zfill(30) + ' zero fill case')
+print(name.lstrip() + ' left strip case')
+print(name.rstrip() + ' right strip case')
+
+
+
+'The word "Varghese" is centered in a 30-character field padded with *, then " center case" is appended.'
