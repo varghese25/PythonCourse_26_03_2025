78db1f8962efef1dbcc3ebdd5ddd37ce564463ef
Varghese Baby <Vbpoulose@gmail.com>
2025-10-14 13:59:47 -0400

    Py: Slicing-Patteren

--- poblemSolvingCode/Solution23.py
+++ poblemSolvingCode/Solution23.py
@@ -1,24 +1,50 @@
-print()
-print()
-print()
+# print()
+# print()
+# print()
 
 
-# one-linear to reverse string in python 
-[::-1]
+# # one-linear to reverse string in python 
+# # Example: reversed_string = your_string[::-1]
 
-x=5
-print(f"Number:(x)")
-# f-String substitute x with its value 5.
+# x=5
+# print(f"Number:(x)")
+# # f-String substitute x with its value 5.
 
-text = "hello"
-print(text.replace("l","p"))
+# text = "hello"
+# print(text.replace("l","p"))
 
-#Output : ??
+# #Output : ??
 
 
-print(any[0,'',False,[]]))
-#output: False
+# print(any[0,'',False,[]])
+# #output: False
 
-print()
-print(23)
-print(danjo)
+# print()
+# print(23)
+# print(danjo)
+
+
+
+#=====================================#
+s = "abcdef"
+print(s[::2])
+
+"""
+Step 3: Let’s walk through the string
+
+Start at index 0:
+
+1️⃣ take letter at index 0 → “a”
+👣 move 2 steps ahead → next index is 2
+
+2️⃣ take letter at index 2 → “c”
+👣 move 2 steps ahead → next index is 4
+
+3️⃣ take letter at index 4 → “e”
+👣 move 2 steps ahead → next index is 6 (but string ends at 5)
+
+Stop.
+
+✅ Collected letters: a, c, e
+
+👉 Output: "ace"""
