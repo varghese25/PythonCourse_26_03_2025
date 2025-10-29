966bd5e7edb5b1bd9c9bb170153249356dc140ea
Varghese Baby <Vbpoulose@gmail.com>
2025-10-28 20:17:22 -0400

    Slicing

---
+++ poblemSolvingCode/Slicing.py
@@ -0,0 +1,6 @@
+name = "Varghese"
+print(name[0:7]) # name[0:7] means from index 0 to index 6 (7-1)
+print(name[0])
+print(name[-1]) # last character 
+print(name[-7]) # first character
+print(name[0:]) # full string
