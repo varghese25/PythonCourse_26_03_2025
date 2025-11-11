e7f5bfba0393fc2dc945fabcd123f481a7503b51
Varghese Baby <Vbpoulose@gmail.com>
2025-11-11 14:17:07 -0500

    Loop vs List-Updated

--- poblemSolvingCode/range.py
+++ poblemSolvingCode/range.py
@@ -1,10 +1,13 @@
 
 #Loop vs List Comprehension
 
+# Using a loop to create a list of squares
 squares = []
 for x in range (5):
     squares.append(x**2)
 print(squares)
 
+
+# Using list comprehension to create a list of squares
 squares = [x**2 for x in range(5)]
 print(squares)
