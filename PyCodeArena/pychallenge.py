b4f6a5f9a706aa7c2aa5d8e1e2f6f80ee6a1b1b6
Varghese Baby <Vbpoulose@gmail.com>
2025-11-17 18:33:45 -0500

    Updated

--- PyCodeArena/pychallenge.py
+++ PyCodeArena/pychallenge.py
@@ -1,13 +1,18 @@
 class Car:
-
-   wheels = 4
-
-c1 = car ()
-c2 = car ()
-
+    wheels = 4
+c1 = Car ()
+c2 = Car ()
 c1.wheels = 6
-
 print(c1.wheels, c2.wheels)
 
 
 #OutPut: "6 4"
+
+
+
+'''Explanation:
+wheels = 4 → class variable, shared by all instances initially.
+
+c1.wheels = 6 → creates a new instance variable wheels for c1, which overrides the class variable only for c1.
+
+c2 doesn’t have an instance variable wheels, so it still uses the class variable, which is 4.'''
