e58035f31613279ff18e8f4aa20cd745df22674c
Varghese Baby <Vbpoulose@gmail.com>
2025-12-02 11:09:30 -0500

    Update range.py

--- poblemSolvingCode/range.py
+++ poblemSolvingCode/range.py
@@ -14,5 +14,27 @@
 
 
 
+for num in range(1,6,2):
+    print(num)
+
+
+"""
+
+explanation 
+
+it print 1,3,5 because range (1,6,2) starts at 1
+increases by two each time and stops before 6
+
+
+
+a.1 2 3 4 5
+b. 1 3 5
+c.  2 4 6
+d. Error 
+
+
+"""
+
+
 print("-----")
 
