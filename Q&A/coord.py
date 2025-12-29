63c6a16bcbec9082e81f323460a00771e260d8a8
Varghese Baby <Vbpoulose@gmail.com>
2025-12-28 19:26:44 -0500

    Py: Coord

---
+++ Q&A/coord.py
@@ -0,0 +1,22 @@
+import copy
+
+class Coord:
+
+     def __init__(self,x, y, z):
+          self.x = x;
+          self.y = y;
+          self.z = z;
+     def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
+     
+coord = Coord(0,0,0)
+c1 = coord
+c2 = copy.copy(coord)
+c3 = copy.deepcopy(coord)
+
+c1.x =1
+c2.y =2
+c3.z =3
+
+print(coord)
+             
