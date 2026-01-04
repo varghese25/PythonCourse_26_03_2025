14dcc793206e9b8f39fb52a7ea1f741009c655e7
Varghese Baby <Vbpoulose@gmail.com>
2026-01-03 19:06:00 -0500

    Folder_StrutureUpdate

---
+++ General_Code/mathOperation.py
@@ -0,0 +1,62 @@
+# >>> a=10
+# >>> b=5
+# >>> a+b
+# 15
+# >>> a-b
+# 5
+# >>> a/b
+# 2.0
+# >>> a/4
+# 2.5
+# >>> b/10
+# 0.5
+# >>> 2+5
+# 7
+# >>> (a+b)*5
+# 75
+# >>> a+b*5
+# 35
+# >>> a=8.78
+# >>> print(round(a))
+# 9
+# >>> a=-5
+# >>> print(abs(a))
+# 5
+# >>> print(pow(a,3))
+# -125
+# >>> print(pow(a,3))
+# -125
+# >>> a=5
+# >>> print(pow(a,3))
+# 125
+
+# ** used to find the power of
+# >>> print(a**3)
+# 125
+# >>> print(a**2)
+# 25
+# >>> print(10**3)
+# 1000
+# >>> a =7
+# >>> b =8
+# >>> print(max(a,b))
+# 8
+# >>> print(min(a,b))
+# 7
+# >>>
+
+import math #math- module
+a = 4.56
+print(math.ceil(a)) # OT Round Value 5
+print(math.floor(a)) # OT 4
+
+print(math.factorial(5))
+print(math.lcm(4))
+print(math.fmod(5,5))
+
+print(a%2)
+print(a/2)
+
+
+
+
