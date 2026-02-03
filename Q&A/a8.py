8154cbdcce82d6caf0bae4e785ff79d83d4b38ce
Varghese Baby <Vbpoulose@gmail.com>
2026-02-03 12:02:21 -0500

    py: CirularReference&Binary

---
+++ Q&A/a8.py
@@ -0,0 +1,12 @@
+# a = []
+# a.append(a)
+# print(a)
+# """
+# By appending 'a' to 'a', we've created a circular reference.
+# python uses[...] to represent these recursive data structures
+# which help prevent your code from falling into an infinite loop.
+
+# """
+
+a = int('11',2)
+print(a)
