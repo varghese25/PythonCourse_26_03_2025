5e3dbb9877d2dc20de251ff35b60098fee0811c2
Varghese Baby <Vbpoulose@gmail.com>
2026-02-04 12:00:03 -0500

    Py:frozenset-Explanation

--- Q&A/9a.py
+++ Q&A/9a.py
@@ -2,3 +2,10 @@ a = frozenset({1,2})
 b = frozenset({'a','b'})
 c = a|b
 print(c)
+'''
+In python, you can combine two frozen sets using the
+union operator '|' and it will produce a new frozenSet.
+* The order of the elements is not guarranteed in sets,
+So your answer might contain the same elements but in a
+different order here.
+'''
