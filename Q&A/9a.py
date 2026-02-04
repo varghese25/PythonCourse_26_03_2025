6d787bb40e54fe5dc7b1dcb3aa81dac9b81a434d
Varghese Baby <Vbpoulose@gmail.com>
2026-02-04 11:52:20 -0500

    Py:frozenset-immutableSets

---
+++ Q&A/9a.py
@@ -0,0 +1,4 @@
+a = frozenset({1,2})
+b = frozenset({'a','b'})
+c = a|b
+print(c)
--- Q&A/a8.py
+++ Q&A/a8.py
@@ -3,7 +3,7 @@ a.append(a)
 print(a)
 
 """By appending 'a' to 'a', we've created a circular reference.
-python uses[...] to represent these recursive data structures
+python uses [...] to represent these recursive data structures
 which help prevent your code from falling into an infinite loop."""
 # a = int('11',2)
 # print(a)
