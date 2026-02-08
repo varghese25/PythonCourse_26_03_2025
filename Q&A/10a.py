e6ee0ef433cf70ae4c0495ce4c5e9e5e0d28c47c
Varghese Baby <Vbpoulose@gmail.com>
2026-02-07 20:07:07 -0500

    Py: SystaxError_PyCode

---
+++ Q&A/10a.py
@@ -0,0 +1,7 @@
+a = 10
+b = "10"
+print(a === b)
+
+""" SyntaxError: invalid syntax
+Unfortunately, the '===' operator does not exist in Python. 
+it stricly operater used in JavaScript for strict equality comparison."""
