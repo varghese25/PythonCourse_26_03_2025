3e6ea2cf6d1a016af9153f3a1cf429bd2f2f713f
Varghese Baby <Vbpoulose@gmail.com>
2026-02-13 11:37:36 -0500

    py:TrueOrFalse

--- Q&A/12a.py
+++ Q&A/12a.py
@@ -1,33 +1,36 @@
-a = [1,2,3]
-b = a
-b = b + [4]
-print(a)
-print(b)
+# a = [1,2,3]
+# b = a
+# b = b + [4]
+# print(a)
+# print(b)
 
-"""b=b+[4] creates a new list a still points to the original list"""
-
-
-
-""" Just a small correction: (4) is an int. Adding a comma (4,) makes it a tuple, which allows tuple concatenation."""
-a = (1,2,3)
-b = a
-b = b + (4,)
-print(a)
-print(b)
-
-# OutPut
-# print(a) -> (1, 2, 3)
-# print(b) ->(1, 2, 3, 4)
-
-# NB: b + (4) Python sees it as tuple + int
-# Tuples can only be concatenated with other tuples, not integers. 
+# """b=b+[4] creates a new list a still points to the original list"""
 
 
-# Set
-a = {1,2,3}
-b = a
-b = b | {4,}
-print(a)
-print(b)
 
-# Key point: | (union) returns a new set; it does not modify in-place. To modify in-place, you would use b |= {4,}
+# """ Just a small correction: (4) is an int. Adding a comma (4,) makes it a tuple, which allows tuple concatenation."""
+# a = (1,2,3)
+# b = a
+# b = b + (4,)
+# print(a)
+# print(b)
+
+# # OutPut
+# # print(a) -> (1, 2, 3)
+# # print(b) ->(1, 2, 3, 4)
+
+# # NB: b + (4) Python sees it as tuple + int
+# # Tuples can only be concatenated with other tuples, not integers. 
+
+
+# # Set
+# a = {1,2,3}
+# b = a
+# b = b | {4,}
+# print(a)
+# print(b)
+
+# # Key point: | (union) returns a new set; it does not modify in-place. To modify in-place, you would use b |= {4,}
+
+
+print(1 == 2 == False)
