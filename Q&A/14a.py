c6af0b8f00dce67178580fbe2dcccb1bfd175b92
Varghese Baby <Vbpoulose@gmail.com>
2026-02-16 19:08:24 -0500

    Py: Division&floatVSInteger

--- Q&A/14a.py
+++ Q&A/14a.py
@@ -2,5 +2,13 @@
 # print(a * 2) # Output: [1, 2, 3, 1, 2, 3] (the list is repeated twice)
 
 
-a= [1,2,3]
-print(a * -1)
+# a= [1,2,3]
+# print(a * -1)
+
+
+a = 25
+b = 5
+c = a /b # Output: 5.0 (the result of division is a float)
+d = a // b # Output: 5 (the result of floor division is an integer)    
+print(c)
+print(d)
