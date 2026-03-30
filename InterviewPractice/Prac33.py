e4dac0882ebf61f5dc4cbc24a9c84ec636f690bb
Varghese Baby <Vbpoulose@gmail.com>
2026-03-30 19:46:26 -0400

    Prac33

---
+++ InterviewPractice/Prac33.py
@@ -0,0 +1,9 @@
+# print(*2*'12',sep='-')
+
+# d ={}
+# print(d.get("x",0)) # .get() returns default if key is missing
+
+
+
+# d = {True: "A", 1:"B"}
+# print(d) # print(d[True]) # True and 1 are same key in dict, so value of 1 is overridden by value of Trues
